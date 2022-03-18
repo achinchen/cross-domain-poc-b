fc5352f6f048e956b539e0b8e4e7204c4b5fc9e9
Chin <greeting.to.chin@gmail.com>
2022-03-18 15:21:06 +0800

    feat(fetch-post): use fetch post to redirect

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,4 @@
 export const SITE_A = 'https://sitea.achin.dev'
 export const ENDPOINT = `${SITE_A}/api/cookie`
 export const REDIRECT_POINT = `${SITE_A}/api/redirect`
+export const REPEATER_POINT = `${SITE_A}/api/repeater`
